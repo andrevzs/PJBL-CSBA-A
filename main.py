7d252075f7370ddc152b8962b6e080b0bcc524ff
Eduardo Barros <69276792+escbarros@users.noreply.github.com>
2021-06-18 17:12:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,55 +2,61 @@
 # André Vinícius Zicka Schmidt
 # Eduardo Scaburi Costa Barros
 # Pedro Eduardo Galvan Moreira
+from typing import TextIO
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+import sqlite3
+import os.path
+
+class Banco:
+    def __init__(self):
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        caminhoBanco = os.path.join(BASE_DIR, "DB_Bichomon.db")
+        self.conexão = sqlite3.connect(caminhoBanco)
+    def recuperaDadosHistoria(self, nv):
+        self.cursor = self.conexão.cursor()
+        self.cursor.execute(F"SELECT * FROM Historia WHERE TIPO = {nv};")
+        historia = self.cursor.fetchall()
+        self.codHist = historia[0][0]
+        self.Hist = historia[0][1]
+        return self.codHist, self.Hist
+    def CadastraPartida(self, nome, cod):
+        self.cursor = self.conexão.cursor()
+        self.cursor.execute(f"INSERT INTO PARTIDA VALUES(NULL, {cod}, \"{nome}\");")
+        dados = self.cursor.fetchall()
+        self.conexão.commit()
+        self.cursor.close()
+        return print(dados)
+    def recuperaListaBicho(self, cod):
+        self.cursor = self.conexão.cursor()
+        self.cursor.execute(f"SELECT * FROM BICHO WHERE CODHISTORIA = {cod};")
+        bichos = self.cursor.fetchall()
+        return print(bichos)
 
 
-class UtilizaBanco:
-    def __init__(self, nomeB):
-        self.nomeBanco          = nomeB
+class Bichomon(BoxLayout):
+    def dificuldade(self, nv):
+        self.nivel = nv
+        if self.nivel == "facil":
+            self.nivel = 3
+        else:
+            self.nivel = 4
+        self.banco = Banco()
+        self.texto = self.banco.recuperaDadosHistoria(self.nivel)
+        self.cod = self.texto[0]
+        self.historia = self.texto[1]
+        return self.texto
+    def partida(self):
+        self.banco = Banco()
+        self.nome = self.ids["Nome"].text
+        self.banco.recuperaListaBicho(self.cod)
+        self.banco.CadastraPartida(self.nome, self.cod)
+        self.ids['hist'].text = f"{self.nome} {self.historia}"
+        
+class PJBLApp(App):
+    def build(self):
+        return Bichomon()
 
+App = PJBLApp()
+App.run()
 
-class Historia:
-    def __init__(self, codH, txtIntro, tipo):
-        self.codHistoria        = codH
-        self.textoIntro         = txtIntro
-        self.tipo               = tipo
-
-
-class Personagem:
-    def __init__(self, nome, ptsEnrg, atkMax, atkMin):
-        self.nome               = nome
-        self.pontosEnergia      = ptsEnrg
-        self.ataqueMax          = atkMax
-        self.ataqueMin          = atkMin
-
-    def Atacar():
-        pass
-
-    def Critico():
-        pass
-
-    def SofreDano(dano):
-        pass
-
-
-class Bicho:
-    def __init__(self, codB, nome, ptsEnrg, atkMax, atkMin):
-        self.codBicho           = codB
-        self.nome               = nome
-        self.pontosEnergia      = ptsEnrg
-        self.ataqueMax          = atkMax
-        self.ataqueMin          = atkMin
-    
-    def Atacar(min, max):
-        pass
-
-    def SofreDano(dano):
-        pass
-
-
-class Partida:
-    def __init__(self, char, hist, mobs, contB):
-        self.personagem         = char
-        self.historia           = hist
-        self.bichos             = mobs
-        self.contadorBatalhas   = contB
